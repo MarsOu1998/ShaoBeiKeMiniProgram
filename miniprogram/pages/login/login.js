f443bd27427d341ef04c67168bb24a319978c2b5
欧世乐 <1148206898@qq.com>
2020-02-01 13:22:23 +0800

    1

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,11 +1,14 @@
 // pages/login/login.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
 
+  },
+  login:function(event){
+    console.log(event);
+    console.log(event.detail.value.inputPassword)
   },
   register:function(){
     wx.navigateTo({
--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -1,66 +1,51 @@
-// pages/register/register.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  register:function(event){
+    console.log(event.detail.value)
+    if (event.detail.value.inputName == ""){
+      wx.showToast({
+        title: '账号不可以为空',
+        icon:'none'
+      })
+    } else if(event.detail.value.inputPassword == ""){
+      wx.showToast({
+        title: '密码不可以为空',
+        icon: 'none'
+      })
+    }else{
+    wx.cloud.callFunction({
+      name:'searchAccount',
+      data:{
+        account:event.detail.value.inputName
+      },
+      success:function(res){
+        console.log(res)
+        if(res.result.data.length==0){
+          console.log("此账号可以使用，可以注册！");
+          wx.cloud.callFunction({
+            name:'register',
+            data:{
+              account: event.detail.value.inputAccount,
+              password: event.detail.value.inputPassword
+            },
+            success:function(res){
+              console.log(res)
+              console.log("注册成功")
+            }
+          })
+        }
+        else{
+          wx.showToast({
+            title: '此账号已存在',
+            icon:'none'
+          })
+        }
+      }
+    })
+    }
   }
+  
 })
