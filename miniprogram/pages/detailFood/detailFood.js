77ae5c6fe8d728fa2caf651b3f31d746de9fc7a1
欧世乐 <1148206898@qq.com>
2020-02-07 10:58:57 +0800

    1

--- miniprogram/pages/detailFood/detailFood.js
+++ miniprogram/pages/detailFood/detailFood.js
@@ -1,6 +1,7 @@
 var app=getApp()
 var food=[];
 var collection=[];
+var like=[];
 Page({
 
   /**
@@ -21,8 +22,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that=this;
+     var that=this;
     collection=[];
+    like=[];
     wx.cloud.callFunction({
       name:'searchById',
       data:{
@@ -35,16 +37,19 @@ Page({
         that.setData({
           food
         })
+        if (app.globalData.isLogin) {
         wx.cloud.callFunction({
           name:'searchAccount',
           data:{
-            account:'烧贝壳'
+            account:app.globalData.userInfo['account']
           },
           success:function(res){
             console.log(res)
               collection=res.result.data[0]['collection'];
+            like = res.result.data[0]['like']
               console.log("collection:"+collection)
-              var i=0;
+            console.log("like:" + like)
+              var i=0,j=0;
               for(i=0;i<collection.length;i++){
                 if(app.globalData.id==collection[i]){
                   that.setData({
@@ -56,6 +61,17 @@ Page({
                   break;
                 }
               }
+              for(j=0;j<like.length;j++){
+                if (app.globalData.id == like[j]) {
+                  that.setData({
+                    addLike: {
+                      add: true,
+                      url: '../img/like02.png'
+                    }
+                  })
+                  break;
+
+              }
             if (i == collection.length){
               that.setData({
                 addSave: {
@@ -64,54 +80,141 @@ Page({
                 }
               })
             }
-              
-            
-          }
+            if(j==like.length){
+              that.setData({
+                addLike: {
+                  add: false,
+                  url: '../img/like01.png'
+                }
+              })
+            }
+          }}
         })
+
+      }
       }
     })
   },
 
 
- 
- 
-  funLike:function(){
-      var add = this.data.addLike.add;
+  funLike: function () {
+    if (!app.globalData.isLogin){
+      wx.showToast({
+        title: '请先登录',
+        icon:'none'
+      })
+    }
+    else
+   { var add = this.data.addLike.add;
+    var that = this;
     //   console.log(add);
-      if(add){
-          this.setData({
-              addLike:{
-                  add:false,
-                  url: '../img/like01.png'
-              }
-          })
-      }else{
-          this.setData({
+    if (add) {
+      for (var i = 0; i < like.length; i++) {
+        if (like[i] == app.globalData.id) {
+          like.splice(i, 1);
+        }
+        wx.cloud.callFunction({
+          name: 'updateUserInfo',
+          data: {
+            _id: app.globalData.userInfo['_id'],
+            like: like
+          },
+          success: function (res) {
+            console.log(res);
+            that.setData({
               addLike: {
-                  add: true,
-                  url: '../img/like02.png'
+                add: false,
+                url: '../img/like01.png'
               }
-          })
+            })
+          }
+        })
       }
-  },
+    } else {
+      like.push(app.globalData.id);
+      console.log(like)
+      wx.cloud.callFunction({
+        name: 'updateUserInfo',
+        data: {
+          _id: app.globalData.userInfo['_id'],
+          like: like
+        },
+        success: function (res) {
+          console.log(res)
+          that.setData({
+            addLike: {
+              add: true,
+              url: '../img/like02.png'
+            }
+          })
+        }
+
+      })
+      
+  }}
+  }
+  ,
+
+
+ 
+ 
+  
   funSave: function () {
+    if (!app.globalData.isLogin){
+      wx.showToast({
+        title: '请先登录',
+        icon:'none'
+      })
+    }
+    else
+    {var that=this;
       var add = this.data.addSave.add;
-    //   console.log(add);
+       //console.log(add);
       if (add) {
-          this.setData({
+        for(var i=0;i<collection.length;i++){
+          if(collection[i]==app.globalData.id){
+            collection.splice(i,1);
+          }
+        }
+        wx.cloud.callFunction({
+          name:'updateUserInfo',
+          data:{
+            _id: app.globalData.userInfo['_id'],
+            collection: collection
+          },
+          success:function(res){
+            console.log(res);
+            that.setData({
               addSave: {
-                  add: false,
-                  url: '../img/save03.png'
+                add: false,
+                url: '../img/save03.png'
               }
-          })
+            })
+          }
+        })
+          
       } else {
-          this.setData({
+        collection.push(app.globalData.id);
+        console.log(collection)
+        wx.cloud.callFunction({
+          name: 'updateUserInfo',
+          data: {
+            _id: app.globalData.userInfo['_id'],
+            collection: collection
+          },
+          success: function (res) {
+            console.log(res)
+            that.setData({
               addSave: {
-                  add: true,
-                  url: '../img/save04.png'
+                add: true,
+                url: '../img/save04.png'
               }
-          })
-      }
+            })
+          }
+        })
+
+          
+      }}
   }
 
 })
